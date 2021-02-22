33dca7908e8040f46dbf8aee6a302ea7af5bee08
jqchiu <jqchiu@yahoo.com>
2021-02-22 15:14:37 -0800

    added bootstrp code

---
+++ wayfarer-react/src/pages/CityPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class CityPage extends React.Component {
+
+  //make api call for cities, should also get the comment id's with it. 
+
+  render () {
+    return (
+      <div className="container">
+
+      </div>
+    )
+  }
+}
+
+export default CityPage;
