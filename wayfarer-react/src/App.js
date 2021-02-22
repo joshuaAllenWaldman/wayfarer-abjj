ef18c3e4602ead0600d02b94b31dcb1d6dae8600
joshuaWaldman <joshuaallenwaldman@gmail.com>
2021-02-22 15:51:02 -0800

    adds html scaffolding for main pages

--- wayfarer-react/src/App.js
+++ wayfarer-react/src/App.js
@@ -6,12 +6,12 @@ import Header from './components/Header'
 
 function App() {
   return (
-   <div className="container">
+  <div className="container">
     <Header />
     <div>
       { routes }
     </div>
-   </div>
+  </div>
   );
 
 }
